1e6a1673d4685a931d274cd7e25ba050750a15db
Justin McNally <justin@kohactive.com>
2016-08-01 18:03:23 -0500

    Session 4: DB Hooks / Validation

--- apps/sockets/models/user.js
+++ apps/sockets/models/user.js
@@ -1,8 +1,31 @@
 import Thinky from '../../db/connection'
+import UUID from 'node-uuid'
 
-export default Thinky.createModel("User", {
+let model = Thinky.createModel("User", {
   id: Thinky.type.string(),
   username: Thinky.type.string(),
   password: Thinky.type.string(),
   token: Thinky.type.string()
 });
+
+model.pre('save', function(next) {
+  if (!this.token) {
+    this.token = UUID.v4();
+  }
+  next();
+});
+
+model.pre('save', function(next) {
+  let self = this;
+  model.filter({username: this.username}).run().then(function(users) {
+    if (users[0] && users[0].id != self.id) {
+      next(new Error("This username is already in use."));
+    }
+    else {
+      next();
+    }
+  });
+});
+
+
+export default model;
--- seed.js
+++ seed.js
@@ -1,9 +1,8 @@
 import User from './apps/sockets/models/user'
 
 let u = new User({
-  username: 'j-mcnally',
-  password: 'test123',
-  token: '234902x09f0asdf90'
+  username: 'testuser',
+  password: 'test123'
 });
 
 u.saveAll().then(function(record) {
